c31ea74715a90ab9789eb2769eabe9ab700f6d6d
Paulo Camilo <pcamilo@I9.innovagency.com>
2019-07-23 11:39:20 +0100

    Removed unused getters and settes

--- src/android/SendIntentPlugin.java
+++ src/android/SendIntentPlugin.java
@@ -157,20 +157,8 @@ public class SendIntentPlugin extends CordovaPlugin {
             this.description = description;
         }
 
-        public String getAction() {
-            return action;
-        }
-
-        public void setAction(String action) {
-            this.action = action;
-        }
-
         public String getDescription() {
             return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
         }
 
         public static Actions getActionByName(String action) {
